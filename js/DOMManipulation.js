759c7d8630863e024425e27bdf0a5c60d766b851
atylaAzfaAlHarits <nimoonnations@gmail.com>
2023-05-28 15:02:07 +0700

    finish DOM Manipulation

--- js/DOMManipulation.js
+++ js/DOMManipulation.js
@@ -23,6 +23,12 @@ getLink[1].setAttribute('target', '__blank');
     const button = document.getElementById('button');
     var isBgBlue = false;
     button.addEventListener('click', function(){
+        button.addEventListener('mouseover', function(){
+            button.style.backgroundColor = 'blue';
+        });
+        button.addEventListener('mouseout', function(){
+            button.style.backgroundColor = '';
+        });
         const groups = document.getElementsByClassName('group');
         if(isBgBlue){
             // localStorage.setItem('theme', 'bg-blue');
@@ -55,6 +61,60 @@ inptToggle.addEventListener('click', function(){
 
 // Start Manipulation Node
 
+// createElement()
+const newElement = document.createElement('h2');
+const newImg = document.createElement('img')
+const text = document.createTextNode('Selamat belajar javascript DOM');
+const subJudul = document.getElementById('subJudul');
+
+newElement.appendChild(text);
+subJudul.appendChild(newImg);
+subJudul.appendChild(newElement);
+const getImg = document.getElementsByTagName('img')[0];
+getImg.setAttribute('id', 'gif');
+const getImgId = document.getElementById('gif');
+
+getImgId.setAttribute('src', 'https://media.giphy.com/media/xXD8sWsSfmY54hDOjS/giphy.gif');
+getImgId.style.width = '200px'
+
+
+
+// createTextNode()
+const newTagP = document.createElement('p');
+const newText = document.createTextNode('Judul Baru');
+const list_2 = document.getElementById('list-2');
+
+newTagP.appendChild(newText);
+list_2.appendChild(newTagP);
+
+
+// insertBefore()
+const liA = document.querySelector('#a li:nth-child(3)');
+const newLi = document.createElement('li');
+const textLiNew = document.createTextNode('List Baru Ditambahkan');
+const parent = liA.parentNode;
+
+newLi.appendChild(textLiNew);
+parent.insertBefore(newLi, liA);
+
+
+// removeChild()
+
+const elmentRemove = document.getElementById('removeMe');
+const parentNode = elmentRemove.parentNode;
+
+parentNode.removeChild(elmentRemove);
+
+
+
+// replaceChild()
+const replHeading = document.getElementById('heading');
+const createHeading = document.createElement('h3');
+createHeading.textContent = "ini adalah heading";
+
+const parentE = replHeading.parentNode;
+
+parentE.replaceChild(createHeading, replHeading)
 
 
 
